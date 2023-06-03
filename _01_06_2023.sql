9676c82042295af2627973467932f7ac0b64f9b2
Rinat Dusumov <rinat_dusumow@int.pl>
2023-06-03 23:30:50 +0200

    Create _01_06_2023.sql

---
+++ _01_06_2023.sql
@@ -0,0 +1,51 @@
+-- Выполнить расчет З/П менеджеров за весь период заказов из расчета ставки в 10%
+select 
+	Employees.LastName,
+    Employees.FirstName,
+    count(*) as total_order,
+    count(*) * 0.1 as salary
+from Orders
+inner join Employees
+on Orders.EmployeeID = Employees.EmployeeID
+group by Orders.EmployeeID;
+
+-- Вывести сотрудников, у которых ко-во заказов менее 20
+select
+	Employees.LastName,
+	Employees.FirstName,
+    count(*) as total_orders
+from Orders 
+inner join Employees
+on Orders.EmployeeID = Employees.EmployeeID
+group by Orders.EmployeeID
+having total_orders <= 20
+order by total_orders desc;
+
+-- Вывести названия компаний-перевозчиков и сколько заказов каждая из них доставила
+select
+	Suppliers.SupplierName,
+    count(*) total_orders
+from OrderDetails
+inner join Products
+on OrderDetails.ProductID = Products.ProductID
+inner join Suppliers
+on Products.SupplierID = Suppliers.SupplierID
+group by OrderDetails.OrderID;
+
+-- Для клиентов из Mexico очистить контактные имена
+set sql_safe_updates = 0;
+update Customers
+set ContactName= ""
+where Country = "Mexico";
+set sql_safe_updates = 1;
+
+-- Описать связи между таблицами, напр., след. образом:
+-- Orders.CustomerID (M:1) Customers.CustomerID
+create table Orders (
+	OrderID integer primary key auto_increment,
+    CustomerID integer,
+    foreign key (CustomerID) references Customers(CustomerID)
+);
+create table Customers (
+	CustomerID integer primary key auto_increment
+);
